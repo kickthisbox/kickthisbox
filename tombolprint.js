f884cffb3f2815fa5ab5869e32db4291f7881de6
kickthisbox <45524592+kickthisbox@users.noreply.github.com>
2018-12-02 10:16:41 +0700

    Add files via upload

---
+++ tombolprint.js
@@ -0,0 +1,4 @@
+<script type='text/javascript'>
+<!--// Tombol Print//-->
+var pfHeaderImgUrl = '';var pfHeaderTagline = '';var pfdisableClickToDel = 0;var pfHideImages = 0;var pfImageDisplayStyle = 'none';var pfDisablePDF = 0;var pfDisableEmail = 1;var pfDisablePrint = 0;var pfCustomCSS = '';var pfBtVersion='2';(function(){var js,pf;pf=document.createElement('script');pf.type='text/javascript';pf.src='//cdn.printfriendly.com/printfriendly.js';document.getElementsByTagName('head')[0].appendChild(pf)})();
+</script>
